53d5332816832344162095ddafc724343de37f6f
chalana kalpitha <chalanakalpitha72@gmail.com>
2022-05-16 13:25:02 +0530

    authentication change

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const cors = require('cors')
-const bookRouter = require('./routes/book-routes')
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bookRouter = require("./routes/book-routes");
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use("/books",bookRouter)
+app.use(express.json());
+app.use(cors());
+app.use("/books", bookRouter);
 
-mongoose.connect('mongodb+srv://chalana:chalana@cluster0.wk9xy.mongodb.net/bookStore?retryWrites=true&w=majority')
-.then(() => console.log("Connected to the database"))
-.then(() => app.listen(5000))
-.catch((err) => {
+mongoose
+  .connect(process.env.MONGODB_TOKEN)
+  .then(() => console.log("Connected to the database"))
+  .then(() => app.listen(5000))
+  .catch((err) => {
     console.log(err);
-})
+  });
